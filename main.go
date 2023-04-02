5809710d2ab6a97fb2331a2bab67546f6f1eb279
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-02 22:30:25 +0900

    팔로잉 JSON 구조체

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import "github.com/labstack/echo/v4"
+
+type User struct {
+	login             string
+	id                int
+	nodeId            string
+	avatarUrl         string
+	gravatarId        string
+	url               string
+	htmlUrl           string
+	followersUrl      string
+	followingUrl      string
+	gistsUrl          string
+	starredUrl        string
+	subscriptionsUrl  string
+	organizationsUrl  string
+	reposUrl          string
+	eventsUrl         string
+	receivedEventsUrl string
+	userType          string
+	siteAdmin         string
+}
 
 func main() {
-	fmt.Println("Hello world")
+	e := echo.New()
+	e.Logger.Fatal(e.Start(":8080"))
 }
