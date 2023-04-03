a46bdea8acd3e368886566419ef35b0b24538d5b
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-03 09:22:07 +0900

    언팔로워 리스트 뽑아오기 성공

--- main.go
+++ main.go
@@ -35,17 +35,14 @@ func main() {
 	//e := echo.New()
 	//e.Logger.Fatal(e.Start(":8080"))
 	var (
-		followingList []User
-		//unFollowerList []User
+		followingList  []User
+		unFollowerList []User
 	)
 	followingList = getFollowUserList("yoochanhong")
-	//unFollowerList = getUnFollowUserList("yoochanhong", followingList)
-	for _, user := range followingList {
+	unFollowerList = getUnFollowUserList("yoochanhong", followingList)
+	for _, user := range unFollowerList {
 		fmt.Println(user.Login)
 	}
-	//for _, user := range unFollowerList {
-	//	fmt.Println(user.Login)
-	//}
 }
 
 // 내가 팔로잉한 사람들을 긁어오는 함수
@@ -55,7 +52,7 @@ func getFollowUserList(userName string) []User {
 		pageURL := baseurl + userName + "/following?per_page=100&page=" + strconv.Itoa(i)
 		req, err := http.NewRequest("GET", pageURL, nil)
 		utils.CheckErr(err)
-		req.Header.Set("Authorization", "Bearer"+token)
+		req.Header.Set("Authorization", "Bearer "+token)
 		client := &http.Client{}
 
 		res, err := client.Do(req)
@@ -74,16 +71,19 @@ func getFollowUserList(userName string) []User {
 }
 
 // 내가 팔로우했지만 나를 팔로우하지 않은 사람들을 모두 가져오는 함수
-//func getUnFollowUserList(userName string, followList []User) []User {
-//	var userList []User
-//	for _, user := range followList {
-//		pageURL := baseurl + user.Login + "/following/" + userName
-//		res, err := http.Get(pageURL)
-//		res.Header.Set("Authorization", "Bearer"+token)
-//		utils.CheckErr(err)
-//		if res.StatusCode == 404 {
-//			userList = append(userList, user)
-//		}
-//	}
-//	return userList
-//}
+func getUnFollowUserList(userName string, followList []User) []User {
+	var userList []User
+	for _, user := range followList {
+		pageURL := baseurl + user.Login + "/following/" + userName
+		req, err := http.NewRequest("GET", pageURL, nil)
+		utils.CheckErr(err)
+		req.Header.Set("Authorization", "Bearer "+token)
+		client := &http.Client{}
+		res, err := client.Do(req)
+		utils.CheckErr(err)
+		if res.StatusCode == 404 {
+			userList = append(userList, user)
+		}
+	}
+	return userList
+}
