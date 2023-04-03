3daa3748e06705c34a1bb638762ccdb87378e423
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-03 22:48:51 +0900

    json 데이터 명시

--- main.go
+++ main.go
@@ -9,24 +9,24 @@ import (
 )
 
 type User struct {
-	Login             string
-	Id                int
-	NodeId            string
-	AvatarUrl         string
-	GravatarId        string
-	Url               string
-	HtmlUrl           string
-	FollowersUrl      string
-	FollowingUrl      string
-	GistsUrl          string
-	StarredUrl        string
-	SubscriptionsUrl  string
-	OrganizationsUrl  string
-	ReposUrl          string
-	EventsUrl         string
-	ReceivedEventsUrl string
-	Type              string
-	SiteAdmin         bool
+	Login             string `json:"login"`
+	ID                int    `json:"id"`
+	NodeID            string `json:"node_id"`
+	AvatarURL         string `json:"avatar_url"`
+	GravatarID        string `json:"gravatar_id"`
+	URL               string `json:"url"`
+	HTMLURL           string `json:"html_url"`
+	FollowersURL      string `json:"followers_url"`
+	FollowingURL      string `json:"following_url"`
+	GistsURL          string `json:"gists_url"`
+	StarredURL        string `json:"starred_url"`
+	SubscriptionsURL  string `json:"subscriptions_url"`
+	OrganizationsURL  string `json:"organizations_url"`
+	ReposURL          string `json:"repos_url"`
+	EventsURL         string `json:"events_url"`
+	ReceivedEventsURL string `json:"received_events_url"`
+	Type              string `json:"type"`
+	SiteAdmin         bool   `json:"site_admin"`
 }
 
 var baseurl = "https://api.github.com/users/"
