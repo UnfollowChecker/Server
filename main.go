c49a54d4706212bef7260a35441a6abd2438d7e3
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-07 14:42:40 +0900

    함수 따로 빼줌

--- main.go
+++ main.go
@@ -16,54 +16,56 @@ func (a User) Less(i, j int) bool { return a[i].Login < a[j].Login }
 func main() {
 
 	e := echo.New()
-	e.GET("/unfollowing", func(c echo.Context) error {
-		userName := c.QueryParam("userName")
-		//unfollowCh := make(chan User)
-		m := make(map[string]int)
-		var (
-			followingList User
-			followersList User
-			list          User
-		)
-		followingNum, followersNum := handler.GetUserFollowInfo(userName)
-		followingList = User(handler.GetFollowUserList(userName, "following", followingNum))
-		followersList = User(handler.GetFollowUserList(userName, "followers", followersNum))
-		for _, user := range followingList {
-			handler.UserSet1(user, m)
-		}
-		for _, user := range followersList {
-			a := handler.FindUnfollwer(user, m)
-			if a == 1 {
-				list = append(list, user)
-			}
-		}
-		sort.Sort(list)
-		return c.JSON(200, list)
-	})
+	e.GET("/unfollowing", unfollowingCheckFunc)
 
-	e.GET("/unfollower", func(c echo.Context) error {
-		userName := c.QueryParam("userName")
-		//unfollowCh := make(chan User)
-		m := make(map[string]int)
-		var (
-			followingList User
-			followersList User
-			list          User
-		)
-		followingNum, followersNum := handler.GetUserFollowInfo(userName)
-		followingList = User(handler.GetFollowUserList(userName, "following", followingNum))
-		followersList = User(handler.GetFollowUserList(userName, "followers", followersNum))
-		for _, user := range followersList {
-			handler.UserSet1(user, m)
-		}
-		for _, user := range followingList {
-			a := handler.FindUnfollwer(user, m)
-			if a == 1 {
-				list = append(list, user)
-			}
-		}
-		sort.Sort(list)
-		return c.JSON(200, list)
-	})
+	e.GET("/unfollower", unfollowerCheckFunc)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func unfollowingCheckFunc(c echo.Context) error {
+	userName := c.QueryParam("userName")
+	m := make(map[string]int)
+	var (
+		followingList User
+		followersList User
+		list          User
+	)
+	followingNum, followersNum := handler.GetUserFollowInfo(userName)
+	followingList = User(handler.GetFollowUserList(userName, "following", followingNum))
+	followersList = User(handler.GetFollowUserList(userName, "followers", followersNum))
+	for _, user := range followingList {
+		handler.UserSet1(user, m)
+	}
+	for _, user := range followersList {
+		a := handler.FindUnfollwer(user, m)
+		if a == 1 {
+			list = append(list, user)
+		}
+	}
+	sort.Sort(list)
+	return c.JSON(200, list)
+}
+
+func unfollowerCheckFunc(c echo.Context) error {
+	userName := c.QueryParam("userName")
+	m := make(map[string]int)
+	var (
+		followingList User
+		followersList User
+		list          User
+	)
+	followingNum, followersNum := handler.GetUserFollowInfo(userName)
+	followingList = User(handler.GetFollowUserList(userName, "following", followingNum))
+	followersList = User(handler.GetFollowUserList(userName, "followers", followersNum))
+	for _, user := range followersList {
+		handler.UserSet1(user, m)
+	}
+	for _, user := range followingList {
+		a := handler.FindUnfollwer(user, m)
+		if a == 1 {
+			list = append(list, user)
+		}
+	}
+	sort.Sort(list)
+	return c.JSON(200, list)
 }
