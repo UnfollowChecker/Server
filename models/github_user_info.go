78b660ada63b8b0e0f21df7049f6266700a2f357
yoochanhong <chanhongy6@naver.com>
2023-05-03 14:28:12 +0900

    Name 속성이 비어있어서 삭제;

--- models/github_user_info.go
+++ models/github_user_info.go
@@ -2,7 +2,6 @@ package models
 
 type GithubUserInfo struct {
 	Login     string `json:"login"`
-	Name      string `json:"name"`
 	AvatarURL string `json:"avatar_url"`
 	HTMLURL   string `json:"html_url"`
 	Type      string `json:"type"`
