cb38f41e059c3e40adda21039ff77dd337c5914a
yoochanhong <chanhongy6@naver.com>
2023-05-03 13:26:17 +0900

    name 속성 추가

--- models/github_user_info.go
+++ models/github_user_info.go
@@ -2,6 +2,7 @@ package models
 
 type GithubUserInfo struct {
 	Login     string `json:"login"`
+	Name      string `json:"name"`
 	AvatarURL string `json:"avatar_url"`
 	HTMLURL   string `json:"html_url"`
 	Type      string `json:"type"`
